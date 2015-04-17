736d43250f662039f9ee0a5af8131b4171a9c747
David Molineus <david.molineus@netzmacht.de>
2015-04-17 10:42:36 +0200

    Create the file first.

--- runonce/create-locallang.php
+++ runonce/create-locallang.php
@@ -7,6 +7,11 @@ class RunOnceController
     public function run()
     {
         if (!file_exists(TL_ROOT . '/system/languages/locallang.php')) {
+            if (!is_dir(TL_ROOT . '/system/languages')) {
+                $files = \Files::getInstance();
+                $files->mkdir('system/languages');
+            }
+
             $file = new \File('system/languages/locallang.php');
             $file->write('<?php' . "\n");
             $file->close();
